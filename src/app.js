810f90fd0074d97b80c9cf90cbdce50a4fb8e41b
Selvan S <87435305+Selvan-S@users.noreply.github.com>
2023-10-03 21:44:42 +0530

    Added hamburger toggle menu app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
 document.getElementById("myform").reset();
+
+const hamburger = document.getElementById('nav-toggle');
+let navMenu = document.getElementById('nav-menu');
+let navList = document.querySelectorAll('.nav__link');
+
+function show_menu(){
+    // To close the menu using hamburger icon.
+    hamburger.addEventListener('click', () =>{
+        navMenu.classList.toggle('show');
+    });
+    // To close the menu, after clicking any link in the menu.
+    navList.forEach(element => {
+       element.addEventListener('click', ()=>{
+        navMenu.classList.toggle('show');
+       }) 
+    });
+}
+show_menu();
